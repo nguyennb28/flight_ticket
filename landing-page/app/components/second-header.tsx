0d77aac4bf1f9544d608d39bfdce2484c311f3e3
nguyennb28 <nguyennb.coding@gmail.com>
2025-09-29 00:19:47 +0700

    make header and footer for homepage

--- landing-page/app/components/second-header.tsx
+++ landing-page/app/components/second-header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FirstPlaneLandingPage from "../assets/images/plane_3.jpg";
 
 const Hero = () => {
   return (
@@ -10,83 +11,23 @@ const Hero = () => {
             <div className="w-full px-4 lg:w-5/12">
               <div className="hero-content">
                 <h1 className="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl">
-                  Kickstart Startup Website with TailGrids
+                  Nơi cập nhật thông tin chuyến bay một cách nhanh nhất, thuận
+                  tiện nhất
                 </h1>
                 <p className="mb-8 max-w-[480px] text-base text-body-color dark:text-dark-6">
-                  With TailGrids, business and students thrive together.
-                  Business can perfectly match their staffing to changing demand
-                  throughout the dayed.
+                  Với website của chúng tôi, các bạn có thể cập nhật thông tin
+                  chuyến bay của sân bay Cát Bi, Hải Phòng.
                 </p>
                 <ul className="flex flex-wrap items-center">
                   <li>
                     <a
                       href="/#"
-                      className="inline-flex items-center justify-center rounded-md bg-primary px-6 py-3 text-center text-base font-medium text-white hover:bg-blue-dark lg:px-7"
+                      className="inline-flex items-center justify-center rounded-md shadow-xl bg-sky-500 px-6 py-3 text-center text-base font-medium text-white hover:bg-blue-dark lg:px-7"
                     >
-                      Get Started
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/#"
-                      className="inline-flex items-center justify-center px-5 py-3 text-center text-base font-medium text-[#464646] hover:text-primary dark:text-white"
-                    >
-                      <span className="mr-2">
-                        <svg
-                          width="24"
-                          height="25"
-                          viewBox="0 0 24 25"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <circle cx="12" cy="12.6152" r="12" fill="#3758F9" />
-                          <rect
-                            x="7.99893"
-                            y="14.979"
-                            width="8.18182"
-                            height="1.63636"
-                            fill="white"
-                          />
-                          <rect
-                            x="11.2717"
-                            y="7.61523"
-                            width="1.63636"
-                            height="4.09091"
-                            fill="white"
-                          />
-                          <path
-                            d="M12.0898 14.1606L14.9241 11.0925H9.25557L12.0898 14.1606Z"
-                            fill="white"
-                          />
-                        </svg>
-                      </span>
-                      Download App
+                      Bắt đầu
                     </a>
                   </li>
                 </ul>
-                <div className="clients pt-16">
-                  <h6 className="mb-6 flex items-center text-xs font-normal text-body-color dark:text-dark-6">
-                    Some Of Our Clients
-                    <span className="ml-3 inline-block h-px w-8 bg-body-color"></span>
-                  </h6>
-
-                  <div className="flex items-center space-x-4">
-                    <SingleImage
-                      href="#"
-                      imgSrc="https://cdn.tailgrids.com/assets/images/marketing/brands/ayroui.svg"
-                    />
-
-                    <SingleImage
-                      href="#"
-                      imgSrc="https://cdn.tailgrids.com/assets/images/marketing/brands/graygrids.svg"
-                    />
-
-                    <SingleImage
-                      href="#"
-                      imgSrc="https://cdn.tailgrids.com/assets/images/marketing/brands/uideck.svg"
-                    />
-                  </div>
-                </div>
               </div>
             </div>
             <div className="hidden px-4 lg:block lg:w-1/12"></div>
@@ -94,9 +35,10 @@ const Hero = () => {
               <div className="lg:ml-auto lg:text-right">
                 <div className="relative z-10 inline-block pt-11 lg:pt-0">
                   <img
-                    src="https://cdn.tailgrids.com/assets/images/marketing/hero/hero-image-01.png"
+                    // src="https://cdn.tailgrids.com/assets/images/marketing/hero/hero-image-01.png"
+                    src={FirstPlaneLandingPage}
                     alt="hero"
-                    className="max-w-full lg:ml-auto"
+                    className="max-w-full lg:ml-auto rounded-2xl shadow-2xl"
                   />
                   <span className="absolute -bottom-8 -left-8 z-[-1]">
                     <svg
@@ -106,31 +48,31 @@ const Hero = () => {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
-                      <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
-                      <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
-                      <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
-                      <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
-                      <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
-                      <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
-                      <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
-                      <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
-                      <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
-                      <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
-                      <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
-                      <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
-                      <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
-                      <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
-                      <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
-                      <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
-                      <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
-                      <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
-                      <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
-                      <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
-                      <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
-                      <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
-                      <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
-                      <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                      `<circle cx="2.5" cy="2.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="2.5" cy="24.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="2.5" cy="46.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="2.5" cy="68.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="2.5" cy="90.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="24.5" cy="2.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="24.5" cy="24.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="24.5" cy="46.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="24.5" cy="68.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="24.5" cy="90.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="46.5" cy="2.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="46.5" cy="24.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="46.5" cy="46.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="46.5" cy="68.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="46.5" cy="90.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="68.5" cy="2.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="68.5" cy="24.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="68.5" cy="46.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="68.5" cy="68.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="68.5" cy="90.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="90.5" cy="2.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="90.5" cy="24.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="90.5" cy="46.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="90.5" cy="68.5" r="2.5" fill="#3e73e5" />
+                      <circle cx="90.5" cy="90.5" r="2.5" fill="#3e73e5" />`
                     </svg>
                   </span>
                 </div>
@@ -196,10 +138,10 @@ const Navbar = () => {
                 } `}
               >
                 <ul className="block lg:flex">
-                  <ListItem NavLink="/#">Home</ListItem>
-                  <ListItem NavLink="/#">Payment</ListItem>
-                  <ListItem NavLink="/#">About</ListItem>
-                  <ListItem NavLink="/#">Blog</ListItem>
+                  <ListItem NavLink="/#">Trang chủ</ListItem>
+                  <ListItem NavLink="/#">Vé</ListItem>
+                  <ListItem NavLink="/#">Về chúng tôi</ListItem>
+                  <ListItem NavLink="/#">Liên hệ</ListItem>
                 </ul>
               </nav>
             </div>
@@ -208,14 +150,14 @@ const Navbar = () => {
                 href="/#"
                 className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
               >
-                Sign in
+                Đăng nhập
               </a>
 
               <a
                 href="/#"
-                className="rounded-lg bg-primary px-7 py-3 text-base font-medium text-white hover:bg-opacity-90"
+                className="rounded-lg bg-cyan-light px-7 py-3 text-base font-medium text-white hover:bg-opacity-90"
               >
-                Sign Up
+                Đăng ký
               </a>
             </div>
           </div>

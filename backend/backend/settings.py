89e5dfb441bb4b2d596ffeab1575f7c63c8b641f
nguyennb28 <nguyennb.coding@gmail.com>
2025-09-29 14:48:11 +0700

    add jwt into backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,8 +84,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    "DEFAULT_PAGINATION_CLASS": "api.pagination.CustomPageNumberPagination",
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # "DEFAULT_PAGINATION_CLASS": "api.pagination.CustomPageNumberPagination",
     # "PAGE_SIZE": 20,
 }
 

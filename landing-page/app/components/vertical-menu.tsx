3c37fece140aa41b29159aadfdabbf0475acc7f3
nguyennb28 <nguyennb.coding@gmail.com>
2025-10-15 08:33:00 +0700

    modify policy

--- landing-page/app/components/vertical-menu.tsx
+++ landing-page/app/components/vertical-menu.tsx
@@ -13,15 +13,6 @@ const VerticalMenu = () => {
 
         <li>
           <a
-            href="#vietnamese-section-one"
-            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-          >
-            Teams
-          </a>
-        </li>
-
-        <li>
-          <a
             href="#vietnamese-section-two"
             className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200"
           >

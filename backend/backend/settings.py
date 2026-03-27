d26577e39e093f01cfb1c194126c162d77820f1d
nguyennb28 <nguyennb.coding@gmail.com>
2026-03-27 10:06:08 +0700

    update env and settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n_d*!+8r84j(*gb)i0sz+7)0$&-%^%-x&l38=nyfe**xll7366"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
